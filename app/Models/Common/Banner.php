f9869af1ddbb57b2f1f69dcdb6f3227458a4c936
Talanoff <talanov.o@gmail.com>
2020-08-19 16:09:39 +0300

    #

--- app/Models/Common/Banner.php
+++ app/Models/Common/Banner.php
@@ -45,7 +45,7 @@ class Banner extends Model implements HasMedia
         $media = asset('images/no-image.png');
 
         if ($this->hasMedia('banners')) {
-            $media = $this->getFirstMedia('banner')->getFullUrl('thumb');
+            $media = $this->getFirstMedia('banners')->getFullUrl('thumb');
         }
 
         return $media;
